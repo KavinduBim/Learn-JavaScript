3f717f6da54f5ccc12f9c445d8fe20ce12a94da3
KavinduBim <63743812+KavinduBim@users.noreply.github.com>
2023-05-10 11:29:35 +0530

    function expression and default value parameter

--- function.js
+++ function.js
@@ -18,13 +18,23 @@ console.log(str);
 
 
 //Passing a value
-function greet3(user)
+function greet3(user)//user is a local variable
 {
     return `Hello ${user}!`
 }
 
-let user = 'Navin';
+let user = 'Navin';//user is a global variable
 let str1 = greet3(user);
 console.log(str1);
+
+//function expression
+let add = function(num1,num2 = 3)
+        {
+            return num1 + num2;
+        }
+
+let sum = add;
+let result = sum(6,7);
+console.log(result);
 
 
