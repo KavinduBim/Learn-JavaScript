5af2a6aacc75ba45b4f121d814fa5cd9b9873a0e
KavinduBim <63743812+KavinduBim@users.noreply.github.com>
2023-05-10 12:49:39 +0530

    objects with methods

--- object.js
+++ object.js
@@ -1,3 +1,4 @@
+/*
 let alien = {
     name : 'Navin',
     tech : 'JS',
@@ -12,3 +13,36 @@ for(let key in alien.laptop){
 
     console.log(key, alien.laptop[key]);
 }
+*/
+
+// object with methods
+let laptop2 = {
+    cpu: 'i7',
+    ram: 16,
+    brand: 'Apple',
+
+    getConfig: function(){
+
+        console.log(this.cpu);
+    }
+}
+
+let laptop1 = {
+    cpu: 'i9',
+    ram: 16,
+    brand: 'HP',
+
+    compare: function(laptop){
+        if(this.cpu > laptop.cpu){
+            console.log(this);
+        }
+        else
+            console.log(laptop);
+
+    }
+}
+
+
+
+laptop2.getConfig();
+laptop1.compare(laptop2);
